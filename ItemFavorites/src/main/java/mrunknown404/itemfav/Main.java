018528deb6e18d949446a216d371a679b743aabe
-Unknown- <MrUnknownYT@gmail.com>
2020-08-12 17:45:44 -0600

    Updated drop logic

--- ItemFavorites/src/main/java/mrunknown404/itemfav/Main.java
+++ ItemFavorites/src/main/java/mrunknown404/itemfav/Main.java
@@ -3,10 +3,14 @@ package mrunknown404.itemfav;
 import java.io.File;
 import java.nio.file.Paths;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.lwjgl.input.Keyboard;
 
 import mrunknown404.itemfav.handler.MasterEventHandler;
+import mrunknown404.itemfav.utils.LockHandler;
 import mrunknown404.itemfav.utils.compat.InvTweaksCompat;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.settings.GameSettings;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
@@ -20,6 +24,18 @@ public class Main {
 	public static final String MOD_ID = "itemfav";
 	
 	public static final KeyBinding KEY_LOCK_ITEM = new KeyBinding("key.lockitem", Keyboard.KEY_R, "key.itemlock.category");
+	public static final KeyBinding KEY_NEW_DROP = new KeyBinding("key.drop", Keyboard.KEY_Q, "key.categories.inventory") {
+		@Override
+		public boolean isActiveAndMatches(int keyCode) {
+			if (mc.player == null && mc.currentScreen != null) {
+				return super.isActiveAndMatches(keyCode);
+			}
+			
+			return !LockHandler.isSlotLocked(mc.player.inventory.currentItem) && super.isActiveAndMatches(keyCode);
+		}
+	};
+	
+	private static Minecraft mc;
 	
 	@Instance
 	public static Main main;
@@ -30,6 +46,13 @@ public class Main {
 	public void preInit(FMLPreInitializationEvent e) {
 		MinecraftForge.EVENT_BUS.register(new MasterEventHandler());
 		ClientRegistry.registerKeyBinding(KEY_LOCK_ITEM);
+		mc = Minecraft.getMinecraft();
+		
+		GameSettings s = mc.gameSettings;
+		s.keyBindings = ArrayUtils.removeElement(s.keyBindings, s.keyBindDrop);
+		s.keyBindings = ArrayUtils.add(s.keyBindings, KEY_NEW_DROP);
+		s.keyBindDrop = KEY_NEW_DROP;
+		s.loadOptions();
 		
 		InvTweaksCompat.dir = Paths.get(e.getModConfigurationDirectory() + "/InvTweaksRules.txt");
 		
--- ItemFavorites/src/main/java/mrunknown404/itemfav/handler/MasterEventHandler.java
+++ ItemFavorites/src/main/java/mrunknown404/itemfav/handler/MasterEventHandler.java
@@ -2,10 +2,6 @@ package mrunknown404.itemfav.handler;
 
 import org.lwjgl.input.Mouse;
 
-import io.netty.channel.ChannelHandler;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelOutboundHandlerAdapter;
-import io.netty.channel.ChannelPromise;
 import mrunknown404.itemfav.Main;
 import mrunknown404.itemfav.client.gui.RenderOverlay;
 import mrunknown404.itemfav.utils.LockHandler;
@@ -15,15 +11,12 @@ import net.minecraft.client.settings.GameSettings;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Slot;
-import net.minecraft.network.play.client.CPacketPlayerDigging;
 import net.minecraftforge.client.event.GuiContainerEvent;
 import net.minecraftforge.client.event.GuiScreenEvent.KeyboardInputEvent;
 import net.minecraftforge.client.event.GuiScreenEvent.MouseInputEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
-import net.minecraftforge.event.entity.item.ItemTossEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.network.FMLNetworkEvent.ClientConnectedToServerEvent;
 
 public class MasterEventHandler {
 	@SubscribeEvent
@@ -88,43 +81,5 @@ public class MasterEventHandler {
 		}
 		
 		RenderOverlay.drawScreen(gui);
-	}
-	
-	@SubscribeEvent
-	public void onItemTossEvent(ItemTossEvent e) {
-		if (LockHandler.isSlotLocked(Minecraft.getMinecraft().player.inventory.currentItem)) {
-			e.setCanceled(true);
-		}
-	}
-	
-	@SubscribeEvent
-	public void onNetworkEstablished(ClientConnectedToServerEvent e) {
-		e.getManager().channel().pipeline().addLast(new ChannelHandler[] { new DropFetcher() });
-	}
-	
-	private static class DropFetcher extends ChannelOutboundHandlerAdapter {
-		@Override
-		public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
-			boolean blocked = false;
-			try {
-				if (!(msg instanceof CPacketPlayerDigging)) {
-					return;
-				}
-				
-				CPacketPlayerDigging.Action action = ((CPacketPlayerDigging) msg).getAction();
-				if (action != CPacketPlayerDigging.Action.DROP_ITEM && action != CPacketPlayerDigging.Action.DROP_ALL_ITEMS) {
-					return;
-				}
-				
-				if (!LockHandler.isSlotLocked(Minecraft.getMinecraft().player.inventory.currentItem)) {
-					return;
-				}
-				blocked = true;
-			} finally {
-				if (!blocked) {
-					super.write(ctx, msg, promise);
-				}
-			}
-		}
 	}
 }
